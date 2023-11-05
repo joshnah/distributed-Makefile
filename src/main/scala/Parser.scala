424bbffa2d65163efdfe69e36621299ee15c5b90
Monica Huynh <monicahuynh@hotmail.fr>
2023-11-05 14:20:37 +0100

    :sparkles: Add user input for makefile path + File class

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,15 +1,32 @@
 package main.scala
 
 import scala.io.Source
+import scala.io.StdIn
 import util.control.Breaks._ // Scala 2.8+
 import scala.collection.mutable.ArrayBuffer
+
+import java.nio.file._;
 
 // Makefile parser
 object Parser {
     def main(args: Array[String]) = {
-        val bufferedSource = Source.fromFile("makefiles/blender_2.59/Makefile")
-        val targets = ArrayBuffer[String]()
-        val prerequisites = ArrayBuffer[String]()
+        println("Enter the makefile path to execute")
+        val makefilePath = readLine()
+        // Handles errors from the user input
+        if (!Files.exists(Paths.get(makefilePath))) {
+            println("The path given is unrecognized.")
+            sys.exit(0)
+        }
+        val usefulChar = if (makefilePath.last == '/') null else '/'
+        val makeSuffix = usefulChar + "Makefile"
+        if (!Files.exists(Paths.get(makefilePath + makeSuffix))) {
+            println("No Makefile found inside the path given.")
+            sys.exit(0)
+        }
+
+        val bufferedSource = Source.fromFile(makefilePath + makeSuffix)
+        val targets = ArrayBuffer[File]()
+        val prerequisites = ArrayBuffer[File]()
         val commands = ArrayBuffer[String]()
         var isRuleContext = false
         var ruleSplitted: Array[String] = null
@@ -36,10 +53,14 @@ object Parser {
                             if (itemHeader == "#") {
                                 break
                             }
+                            val filePath = makefilePath+usefulChar+itemHeader
+                            val fileExists = Files.exists(Paths.get(filePath))
+                            // TODO: check if it's better to convert into millis (or alternatives..) for comparison.
+                            val modified =  if (fileExists) Files.getLastModifiedTime(Paths.get(filePath)) else null
                             if (i == 0) {
-                                targets += itemHeader
+                                targets += new File(itemHeader, modified)
                             } else {
-                                prerequisites += itemHeader
+                                prerequisites += new File(itemHeader, modified)
                             }
                         }
                     }
@@ -47,7 +68,7 @@ object Parser {
                     // Found a command before a target. Stop the parsing.
                     if (!isRuleContext) {
                         println(s"makefile:${index+1}: *** commands commence before first target. Stop.")
-                        break
+                        sys.exit(0)
                     }
                     // A command has been detected.
                     if (!line.isEmpty && line.charAt(0) == '\t') {
--- src/main/scala/Rule.scala
+++ src/main/scala/Rule.scala
@@ -1,3 +1,6 @@
 package main.scala
 
-class Rule(val targets: List[String], val dependencies: List[String], val commands: List[String])
+import java.nio.file.attribute.FileTime;
+
+class File(val name: String, val lastModified: FileTime)
+class Rule(val targets: List[File], val dependencies: List[File], val commands: List[String])

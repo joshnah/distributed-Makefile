9aaeda5250394318c355e41a32877fa298219eed
joshnah <tiendatamser296@gmail.com>
2023-12-07 13:04:35 +0100

    revert fix make file prof

--- grid5000/ssh-tunnel.sh
+++ grid5000/ssh-tunnel.sh
@@ -5,3 +5,4 @@ fi
 ssh tiphan@access.grid5000.fr -N -f -L 8080:$1:8080
 ssh tiphan@access.grid5000.fr -N -f -L 7077:$1:7077
 ssh tiphan@access.grid5000.fr -N -f -L 8081:$2:8081
+ssh tiphan@access.grid5000.fr -N -f -L 4040:$1:4040

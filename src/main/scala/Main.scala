62747be254fc86161f75240a164074255978d768
joshnah <tiendatamser296@gmail.com>
2023-12-09 20:02:16 +0100

    remove log from prog

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import scala.collection.mutable.ArrayBuffer
 import sys.process.Process
+import org.apache.log4j.{Level, Logger}
+
 
 import java.nio.file.Paths
 import java.nio.file.Files
@@ -75,14 +77,16 @@ object Main {
         // }
 
         val scheduling = makefile.calc_scheduling(initialTargets.toArray)
-        // println(s"scheduling (${scheduling.length}):")
-        // for (case (targets, index) <- scheduling.zipWithIndex) {
-        //     println(s"#$index: ${targets.map(_.name).mkString(", ")}")
-        // }
+        println("### Scheduling ${scheduling.length} in total ###")
+        for (case (targets, index) <- scheduling.zipWithIndex) {
+            println(s"Step $index: ${targets.map(_.name).mkString(", ")}\n")
+        }
+        println("### End Scheduling ###\n")
 
+        println("### Run ###")
         val runDir = makefile.directory.toFile()
         def run(command: String) = {
-            println(command)
+            println(s"executing: $command")
             val exitCode = Process(Seq("bash", "-c", command), runDir).!
             if (exitCode != 0) {
                 Console.err.println(s"error: command failed with exit code $exitCode: $command")
@@ -92,12 +96,13 @@ object Main {
 
         sparkMasterUrl match {
             case Some(masterUrl) => {
-
                 val conf = new SparkConf()
                     .setAppName("Spark Makefile")
                     .setMaster(masterUrl)
+                    .set("spark.log.level", "ERROR")
 
                 val sc = new SparkContext(conf)
+                val startTime = System.currentTimeMillis()
 
                 // iterate over scheduling and execute each target
                 for (level <- scheduling) {
@@ -107,15 +112,24 @@ object Main {
                     })
                     
                 }
+                // Record the end time
+                val endTime = System.currentTimeMillis()
+
+                // Calculate and print the execution time
+                val executionTime = endTime - startTime
+                println(s"Execution time: $executionTime milliseconds")
+                sc.stop()
+
+                println("### End Run ###\n")
 
             }
             case None => {
 
-                for (level <- scheduling) {
-                    for (target <- level) {
-                        target.commands.foreach(run)
-                    }
-                }
+                // for (level <- scheduling) {
+                //     for (target <- level) {
+                //         target.commands.foreach(run)
+                //     }
+                // }
 
             }
         }

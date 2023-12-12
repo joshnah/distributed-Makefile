412b8ff35de94babf602fbf946c75097a58b75eb
theor <contact@theorozier.fr>
2023-12-12 18:08:14 +0100

    Try logging

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkConf, SparkContext, TaskContext}
 import scala.collection.mutable.ArrayBuffer
 import sys.process.Process
 import org.apache.log4j.{Level, Logger}
@@ -84,9 +84,9 @@ object Main {
         println("### End Scheduling ###\n")
 
         println("### Run ###")
+
         val runDir = makefile.directory.toFile()
         def run(command: String) = {
-            println(s"executing: $command")
             val exitCode = Process(Seq("bash", "-c", command), runDir).!
             if (exitCode != 0) {
                 Console.err.println(s"error: command failed with exit code $exitCode: $command")
@@ -96,29 +96,57 @@ object Main {
 
         sparkMasterUrl match {
             case Some(masterUrl) => {
+
                 val conf = new SparkConf()
                     .setAppName("Spark Makefile")
                     .setMaster(masterUrl)
                     .set("spark.log.level", "ERROR")
 
-                val sc = new SparkContext(conf)
+                val driverCtx = new SparkContext(conf)
                 val startTime = System.currentTimeMillis()
+
+                val acc = driverCtx.collectionAccumulator[String]("Logs")
 
                 // iterate over scheduling and execute each target
                 for (level <- scheduling) {
-                    val rdd = sc.parallelize(level.map(_.commands)) // transmet all the commands of the level
-                    rdd.foreach(commands => {
-                        commands.foreach(run)
-                    })
                     
+                    val rdd = driverCtx.parallelize(level.map(_.commands)) // transmet all the commands of the level
+                    val future = rdd.foreachAsync(commands => {
+
+                        val taskCtx = TaskContext.get()
+                        val id = taskCtx.taskAttemptId()
+                        
+                        commands.foreach(command => {
+                            
+                            acc.add(s"$id: $command")
+
+                            val exitCode = Process(Seq("bash", "-c", command), runDir).!
+                            if (exitCode != 0) {
+                                acc.add(s"$id: error: command failed with exit code $exitCode: $command")
+                                sys.exit(1)
+                            }
+                            
+                        })
+                    })
+
+                    var i = 0;
+                    while (!future.isCompleted) {
+                        while (i < acc.value.size) {
+                            val log = acc.value.get(i);
+                            println(log)
+                            i += 1;
+                        }
+                    }
+
                 }
+
                 // Record the end time
                 val endTime = System.currentTimeMillis()
 
                 // Calculate and print the execution time
                 val executionTime = endTime - startTime
                 println(s"Execution time: $executionTime milliseconds")
-                sc.stop()
+                driverCtx.stop()
 
                 println("### End Run ###\n")
 

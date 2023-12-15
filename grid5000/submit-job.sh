4a977b470cb36117d436c1bc70f2f59adeade2f8
joshnah <tiendatamser296@gmail.com>
2023-12-15 20:13:48 +0100

    suppress output spark

--- grid5000/submit-job.sh
+++ grid5000/submit-job.sh
@@ -1,6 +1,7 @@
 if [ -z "$1" ]
 then
-    ssh $(cat ~/master_node) "/opt/spark-3.5.0-bin-hadoop3/bin/spark-submit /opt/spark-3.5.0-bin-hadoop3/distributed-makefile_2.12-0.1.0-SNAPSHOT.jar -m spark://$(cat ~/master_node):7077" < /dev/null
+    ssh $(cat ~/master_node) "/opt/spark-3.5.0-bin-hadoop3/bin/spark-submit /opt/spark-3.5.0-bin-hadoop3/distributed-makefile_2.12-0.1.0-SNAPSHOT.jar -m spark://$(cat ~/master_node):7077" < /dev/null 2>/dev/null
+
 else
-    ssh $(cat ~/master_node) "/opt/spark-3.5.0-bin-hadoop3/bin/spark-submit /opt/spark-3.5.0-bin-hadoop3/distributed-makefile_2.12-0.1.0-SNAPSHOT.jar -m spark://$(cat ~/master_node):7077 -f $1" < /dev/null
+    ssh $(cat ~/master_node) "/opt/spark-3.5.0-bin-hadoop3/bin/spark-submit /opt/spark-3.5.0-bin-hadoop3/distributed-makefile_2.12-0.1.0-SNAPSHOT.jar -m spark://$(cat ~/master_node):7077 -f $1" < /dev/null  2> /dev/null
 fi

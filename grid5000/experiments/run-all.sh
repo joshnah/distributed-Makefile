2e13116cba6f1f54be72a0f0f0122b8c55830c9c
joshnah <tiendatamser296@gmail.com>
2023-12-18 20:11:48 +0100

    fix script

--- grid5000/experiments/run-all.sh
+++ grid5000/experiments/run-all.sh
@@ -3,7 +3,7 @@ NB_ATTEMPTS=10
 # run experiment with each number of nodes on arguments
 for nb_nodes in "$@" 
 do
-    $folder/setup_make.sh $nb_nodes
+    $folder/../setup_make.sh $nb_nodes
     $folder/run_experiment_matrix.sh NB_ATTEMPTS
     $folder/run_experiment_premier_small.sh NB_ATTEMPTS
     $folder/run_experiment_premier.sh NB_ATTEMPTS
